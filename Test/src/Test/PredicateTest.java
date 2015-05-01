c3d13d89e873a54df70bb6e385ece6ec51092d5f
cgajendrareddy <cgajendrareddy@gmail.com>
2015-05-01 19:49:51 +0530

    Test

---
+++ Test/src/Test/PredicateTest.java
@@ -0,0 +1,22 @@
+package Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * Created by cgajendra-0220 on 5/1/15.
+ */
+public class PredicateTest<T> {
+    public List<T> testPredicate(Predicate<T> p,List<T> list){
+        List<T> returnList=new ArrayList<>();
+        for (T t: list)
+        {
+            if(p.test(t)){
+                returnList.add(t);
+            }
+        }
+        return returnList;
+    }
+
+}
--- Test/src/Test/Test.java
+++ Test/src/Test/Test.java
@@ -1,5 +1,8 @@
 package Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by cgajendra-0220 on 5/1/15.
  */
@@ -7,7 +10,12 @@ public class Test {
 
     public static void main (String[] args)
     {
-        System.out.println("Hello World");
+       List<Integer> intList= Arrays.asList(0, 1, 2, 3, 4, 5);
+        PredicateTest<Integer> test=new PredicateTest<>();
+        System.out.println("all nos:"+test.testPredicate(x-> true,intList));
+        System.out.println("even nos:"+test.testPredicate(x->(x&1)==0,intList));
+        System.out.println("even nos:"+test.testPredicate(x->(x%2)==1,intList));
+
     }
 }
 
